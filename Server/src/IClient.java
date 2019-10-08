efaaf98e5857e7d1ec7b744e5f308247bcf5872d
Zach Goldberg <zag17@txstate.edu>
2019-10-08 15:59:03 -0500

    Added Server to demo branch

---
+++ Server/src/IClient.java
@@ -0,0 +1,13 @@
+public interface IClient{
+	//Send to client from server
+	public void send(String data);
+
+	//Runs the client
+	public void run();
+
+	//Stops the client
+	public void stop();
+
+	public void process(String data);
+
+}
---
+++ Server/src/ServerSideClient.java
@@ -0,0 +1,57 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class ServerSideClient implements IClient, Runnable{
+	private IServer server;
+	private boolean running;
+	private BufferedReader in;
+	private PrintWriter out;
+	private Socket clientSocket;
+	private String IP;
+
+	public ServerSideClient(IServer server, Socket socket){
+		try{
+			this.server = server;
+			clientSocket = socket;
+			IP = clientSocket.getRemoteSocketAddress().toString();
+			running = true;
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			out = new PrintWriter(clientSocket.getOutputStream(),true);
+		}catch(Exception e){System.out.println("ServerSideClient | ERR: " + e.getStackTrace()[1].getLineNumber());}
+	}
+
+	public void send(String data){
+		System.out.println(data);
+		out.println(data);
+	}
+
+	public void run(){
+		while(running){
+			try{
+				process(in.readLine());
+			}catch(Exception e){System.out.println("ServerSideClient.run | Socket closed!"); stop();}
+		}
+	}
+
+	public void stop(){
+		try{
+			running = false;
+			in.close();
+			out.close();
+			clientSocket.close();
+			System.out.println(getIP() +" has disconnected!");
+		}catch(Exception e){System.out.println("ServerSideClient.stop | ERR: " + e.getStackTrace()[1].getLineNumber());}
+	}
+
+	public String getIP(){
+		return IP;
+	}
+
+	public void process(String data){
+		server.broadcast(data);
+		System.out.println(IP + ": " + data);
+
+	}
+
+}
