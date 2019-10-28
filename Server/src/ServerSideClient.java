38ddf747f76d0a467075521aed65f9fe3ccefa1d
PWParker01 <54680917+PWParker01@users.noreply.github.com>
2019-10-28 18:57:05 -0500

    updating ServerSideClient
    adding object input and output

--- Server/src/ServerSideClient.java
+++ Server/src/ServerSideClient.java
@@ -9,6 +9,9 @@ public class ServerSideClient implements IClient, Runnable{
 	private PrintWriter out;
 	private Socket clientSocket;
 	private String IP;
+	
+	private ObjectOutputStream oos;
+	private ObjectInputStream ois;
 
 	public ServerSideClient(IServer server, Socket socket){
 		try{
@@ -18,18 +21,23 @@ public class ServerSideClient implements IClient, Runnable{
 			running = true;
 			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			out = new PrintWriter(clientSocket.getOutputStream(),true);
+			
+			oos = new ObjectOutputStream(clientSocket.getOutputStream());
+			ois - new ObjectInputStream(clientSocket.getInputStream());
 		}catch(Exception e){System.out.println("ServerSideClient | ERR: " + e.getStackTrace()[1].getLineNumber());}
 	}
 
 	public void send(String data){
 		System.out.println(data);
 		out.println(data);
+		oos.writeObject(data);
 	}
 
 	public void run(){
 		while(running){
 			try{
 				process(in.readLine());
+				process((String)ois.readObject());
 			}catch(Exception e){System.out.println("ServerSideClient.run | Socket closed!"); stop();}
 		}
 	}
@@ -39,6 +47,8 @@ public class ServerSideClient implements IClient, Runnable{
 			running = false;
 			in.close();
 			out.close();
+			oos.close();
+			ois.close();
 			clientSocket.close();
 			System.out.println(getIP() +" has disconnected!");
 		}catch(Exception e){System.out.println("ServerSideClient.stop | ERR: " + e.getStackTrace()[1].getLineNumber());}
